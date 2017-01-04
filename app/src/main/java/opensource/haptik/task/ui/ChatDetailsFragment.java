95348aa46376e76b5a2458f1f11dece010647fc8
Rajan Maurya <rajanmaurya154@gmail.com>
2017-01-04 20:42:22 +0530

    Material Icons Added

--- app/src/main/java/opensource/haptik/task/ui/ChatDetailsFragment.java
+++ app/src/main/java/opensource/haptik/task/ui/ChatDetailsFragment.java
@@ -44,7 +44,7 @@ public class ChatDetailsFragment extends Fragment implements UpdateChatDetails {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_chat, container, false);
+        rootView = inflater.inflate(R.layout.fragment_chat_details, container, false);
         ButterKnife.bind(this, rootView);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
